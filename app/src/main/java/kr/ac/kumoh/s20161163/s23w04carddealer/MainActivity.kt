43b17f6eeb15c4d71ebe7e32ad8ae306e6a9edea
hojang <109591135+26solitude@users.noreply.github.com>
2023-10-10 12:29:58 +0900

    .

--- app/src/main/java/kr/ac/kumoh/s20161163/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20161163/s23w04carddealer/MainActivity.kt
@@ -2,10 +2,18 @@ package kr.ac.kumoh.s20161163.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20161163.s23w04carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+
+        main.imgCard1.setImageResource(R.drawable.c_10_of_hearts)
+
     }
 }
