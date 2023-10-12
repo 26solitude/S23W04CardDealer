3f6f46cddfa9f8f4c9fc47fedbdae18f55973281
hojang <109591135+26solitude@users.noreply.github.com>
2023-10-12 17:04:56 +0900

    .

--- app/src/main/java/kr/ac/kumoh/s20161163/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20161163/s23w04carddealer/MainActivity.kt
@@ -13,7 +13,31 @@ class MainActivity : AppCompatActivity() {
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        main.imgCard1.setImageResource(R.drawable.c_10_of_hearts)
+        val res = resources.getIdentifier(getCardName(37), "drawable", packageName) // TODO : 하드 코딩 제거할 것
+        main.imgCard1.setImageResource(res)
+    }
 
+    private fun getCardName(c: Int) : String{
+        val shape = when (c / 13){
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c%13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+
+        return if (number in arrayOf("jack", "queen", "king"))
+            "c_${number}_of_${shape}2"
+        else
+            "c_${number}_of_${shape}"
     }
 }
